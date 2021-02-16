cfd928f973e920dc320b9436320c55458359a493
GMerencio <gmerencio.santos@gmail.com>
2021-02-16 20:56:47 -0300

    Método carregar_dados implementado

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from model import Time, Jogo
 
-def carregar_dados(arquivo):
+def carregar_dados(caminho):
     """
     Retorna uma lista de objetos Time criados a partir do arquivo de texto
-    passado como argumento. A lista é ordenada pelo atributo pontos, em ordem
-    decrescente. Cada linha do arquivo deve conter um resultado de partida
-    no seguinte formato:
+    cujo caminho é passado como argumento. A lista é ordenada pelo atributo
+    pontos, em ordem decrescente. Cada linha do arquivo deve conter um
+    resultado de partida no seguinte formato:
 
     DD/MM/YYYY - HH:MM - time1 AxB time2
 
@@ -14,8 +14,34 @@ def carregar_dados(arquivo):
     a quantidade de gols do segundo time.
     """
     times = {}
-    times['Corint'] = Time('Corint', 4, 1, 1)
-    times['Palm'] = Time('Palm', 10, 0, 0)
+
+    with open(caminho, 'r') as arquivo:
+        for linha in arquivo:
+            # Desconsiderar os primeiros 21 caracteres da linha, bem como
+            # espaço em branco no final.
+            linha = linha.rstrip()[21:]
+            print(linha)
+
+            linha_split = linha.split(' ')
+
+            # Desconsiderar linhas que não estiverem no formato esperado.
+            if len(linha_split) != 3:
+                continue
+
+            nome1, placar, nome2 = linha_split
+            gols1, gols2 = [int(gols) for gols in placar.split('x')]
+
+            if nome1 not in times:
+                times[nome1] = Time(nome1)
+            if nome2 not in times:
+                times[nome2] = Time(nome2)
+
+            # Inserir os resultados em um objeto Jogo e obter os objetos Time
+            # atualizados.
+            jogo = Jogo(times[nome1], times[nome2], gols1, gols2)
+            times[nome1] = jogo.get_time(nome1)
+            times[nome2] = jogo.get_time(nome2)
+    
     return sorted(times.values(), key = lambda time: time.pontos,
                   reverse = True)
 
